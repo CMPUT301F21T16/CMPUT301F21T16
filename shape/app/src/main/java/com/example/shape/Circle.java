7ebd95503944c6d0a86fe31fae0500710f1a66b7
Frank-1998 <ziang6@ualberta.ca>
2021-10-04 12:17:09 -0600

    Add Circle class

--- shape/app/src/main/java/com/example/shape/Circle.java
+++ shape/app/src/main/java/com/example/shape/Circle.java
@@ -1,2 +1,8 @@
-package com.example.shape;public class Circle {
+package com.example.shape;
+
+public class Circle extends Shape{
+
+    public Circle(int x, int y) {
+        super(x, y);
+    }
 }
