9d42ac71dd7592880f121eee843f51a0510599da
anujpradeep <apradeep@ualberta.ca>
2021-10-04 13:16:47 -0600

    Update Shape.java

--- shape/app/src/main/java/com/example/shape/Shape.java
+++ shape/app/src/main/java/com/example/shape/Shape.java
@@ -3,11 +3,7 @@ package com.example.shape;
 public abstract class Shape {
     int x;
     int y;
-<<<<<<< Updated upstream
     String color = "green";
-=======
-    private String color = "blue";
->>>>>>> Stashed changes
     public Shape(int x, int y) {
     }
 }
