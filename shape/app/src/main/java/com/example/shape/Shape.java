aa82db1c4a15d78ad01b841608de0b92d28850e9
anujpradeep <apradeep@ualberta.ca>
2021-10-04 12:01:05 -0600

    added Color to Shape.java

--- shape/app/src/main/java/com/example/shape/Shape.java
+++ shape/app/src/main/java/com/example/shape/Shape.java
@@ -3,6 +3,8 @@ package com.example.shape;
 public abstract class Shape {
     int x;
     int y;
-    public Shape(int x, int y) {
+    String color;
+    public Shape(int x, int y, String color) {
+        this.color = "Blue";
     }
 }
--- shape/app/src/main/java/com/example/shape/Star.java
+++ shape/app/src/main/java/com/example/shape/Star.java
@@ -3,11 +3,9 @@ package com.example.shape;
 public class Star extends Shape{
     private int x;
     private int y;
-    String color;
     public Star(int x, int y, String color) {
-        super(x,y);
+        super(x,y, color);
         this.x = x;
         this.y = y;
-        this.color = color;
     }
 }
